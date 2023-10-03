2097903e26fd2bc73b425da3cf729a2879c1264a
Samiul Alim <xo.ot2001@gmail.com>
2023-10-03 19:27:54 +0600

    Added Route Controllers

--- src/controllers/parseController.ts
+++ src/controllers/parseController.ts
@@ -1,5 +1,11 @@
-const parseController = async (req: any, res: any, next: any) => {
-  res.status(200).json({ message: "API Working!!" });
+import { Scrape } from "../scraper/Scraper.ts";
+
+const parseController = async (req: any, res: any) => {
+  const { url } = req.body;
+
+  const data = await Scrape([url]);
+
+  res.status(200).json({ ...data });
 };
 
 export default parseController;
