61b2a0e2a2d23f28469d233e6fef42c9914f5906
Samiul Alim <xo.ot2001@gmail.com>
2023-10-05 11:02:22 +0600

    Added Varient Image Downloading

--- src/utils/getImages.ts
+++ src/utils/getImages.ts
@@ -3,35 +3,13 @@ import * as fs from "node:fs";
 import path from "node:path";
 import ExifTransformer from "exif-be-gone";
 import { pipeline } from "node:stream/promises";
+import { varient } from "../interfaces/scraper";
 
 // Extract the Image Url from the provided Links.
 export const extractImageUrl = (url: string) => {
   const link: string = url.replace(/\.jpg.*/, ".jpg");
   return link;
 };
-
-// Download Images and Save them to folder
-// const downloadImage = async (
-//   url: string,
-//   i: number,
-//   location: string,
-//   extra?: string
-// ) => {
-//   const fileLocation = path.resolve(
-//     location,
-//     extra ? extra : "",
-//     `image${i}.jpeg`
-//   );
-//   await axios({
-//     method: "get",
-//     url: url,
-//     responseType: "stream",
-//   }).then(function (response) {
-//     response.data
-//       .pipe(new ExifTransformer())
-//       .pipe(fs.createWriteStream(fileLocation));
-//   });
-// };
 
 //The Main Function to handle everything.
 export const getImages = async (
@@ -58,4 +36,24 @@ export const getImages = async (
       fs.createWriteStream(fileLocation)
     );
   }
+};
+
+export const getVarientImage = async (
+  url: string,
+  folder: string,
+  name: string
+) => {
+  const fileLocation = path.resolve(folder, `${name}.jpeg`);
+
+  const response = await axios({
+    method: "get",
+    url: extractImageUrl(url),
+    responseType: "stream",
+  });
+
+  await pipeline(
+    response.data,
+    new ExifTransformer(),
+    fs.createWriteStream(fileLocation)
+  );
 };
