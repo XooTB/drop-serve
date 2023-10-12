82144d3ee66f2fb32d50871c7bbc973c260312b5
Samiul Alim <xo.ot2001@gmail.com>
2023-10-12 21:55:39 +0600

    Added Job Deletion Routes

--- src/controllers/JobControllers.ts
+++ src/controllers/JobControllers.ts
@@ -1,6 +1,7 @@
 import DBHandler from "../handlers/DB.js";
 import JobModel from "../database/models/Job.js";
 import UserModel from "../database/models/user.js";
+import JobDataModel from "../database/models/JobData.js";
 
 export const checkJobStatus = async (req: any, res: any) => {
   const id = req.params.id;
@@ -65,5 +66,54 @@ export const jobController = async (req: any, res: any) => {
     res.status(500).json({
       error: error.message,
     });
+  }
+};
+
+export const deleteJob = async (req: any, res: any) => {
+  const id = req.params.id;
+  const user = req.user;
+
+  try {
+    const job = await JobModel.findOne({ ID: id });
+    if (!job) {
+      res.status(404).json({
+        message: "No Job found. Invalid ID.",
+      });
+      return;
+    }
+    if (job.user?.toString() !== user._id.toString()) {
+      res.status(401).json({
+        message: "You aren't authorized to access this job.",
+      });
+      return;
+    }
+
+    const jobUser = await UserModel.findOne({ _id: job.user });
+
+    if (!jobUser) {
+      throw Error("User for this Job doesn't exists.");
+    }
+    // Delete the Job Data first.
+    if (job.data) {
+      await JobDataModel.findOneAndDelete({ _id: job.data });
+    }
+
+    // Remove the Job from the jobs array.
+    const filteredJobs = jobUser?.jobs.filter(
+      (el) => el.toString() !== job._id.toString()
+    );
+    jobUser.jobs = filteredJobs;
+
+    // Update the user's jobs.
+    await jobUser.save();
+
+    // Remove the Job itself.
+    await job.deleteOne();
+
+    res.status(200).json({
+      message: `The Job was deleted successfully. ID: ${id}`,
+    });
+  } catch (error: any) {
+    console.log(error.message);
   }
 };
--- src/routes/job.route.ts
+++ src/routes/job.route.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { checkJobStatus, getJobData } from "../controllers/JobControllers.js";
+import {
+  checkJobStatus,
+  getJobData,
+  deleteJob,
+} from "../controllers/JobControllers.js";
 import Authenticate from "../controllers/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.get("/status/:id", Authenticate, checkJobStatus);
 router.get("/:id", Authenticate, getJobData);
+router.delete("/delete/:id", Authenticate, deleteJob);
 
 export default router;
